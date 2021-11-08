72013ae948eccf6270dd420a18651f8c34ff2a74
rick5678 <henriquefrancoaraujo@gmail.com>
2021-11-08 20:25:57 -0300

    second commit

--- index.py
+++ index.py
@@ -1,22 +1,24 @@
 from flask import Flask, render_template
 from niveis import nvl1, nvl2, nvl3
+from usuarios import usuario1, usuario2, usuario3
 
 app = Flask(__name__)
 
+logado = usuario1
 
 @app.route('/nvlum')
 def primeiro():
     
-    return render_template('nivel-um.html',  produtora=nvl1, usuario="nvl-2")
+    return render_template('nivel-um.html',  produtora=nvl1, usuario=logado)
 
 @app.route('/nvldois')
 def segundo():
     
-    return render_template('nivel-dois.html',  produtora=nvl2)
+    return render_template('nivel-dois.html',  produtora=nvl2, usuario=logado)
 
 @app.route('/nvltres')
 def terceiro():
     
-    return render_template('nivel-tres.html',  produtora=nvl3)
+    return render_template('nivel-tres.html',  produtora=nvl3, usuario=logado)
 
 app.run(debug=True)
---
+++ usuarios.py
@@ -0,0 +1,8 @@
+class Usuario:
+    def __init__(self, nome, nivel):
+        self.nome = nome
+        self.nivel = nivel 
+
+usuario1 = Usuario('Isaque', '2')
+usuario2 = Usuario('Arion', '1')
+usuario3 = Usuario('Henrique', '3')
